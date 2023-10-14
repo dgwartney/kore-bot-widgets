e417d28ea3711c590de0f2b81cbbc35dec9cd24e
David Gwartney <david.gwartney@kore.com>
2023-10-13 19:17:04 -0700

    Clean unused node_modules

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
 // import chalk from "chalk";
 import Buttons from "./button";
-
-
-export function createButton() {
-    return new Buttons();
-}
-
-const b = createButton();
-console.log(b.render());
+export {Buttons}
